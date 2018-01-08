1a3b9bc02faf157e59dc62c9fd7edf5912de8391
Brian  Lehto <bdlden1978@outlook.com>
2018-01-08 04:25:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function deleteFromObjectByKey(object, key) {
    delete newObj[key];
    return newObj
 }
-function destructivelyDeleteFromObjectByKey(object, key) {
-  return delete object.key;
+function destructivelyDeleteFromObjectByKey(object, key){
+   delete object.key;
+   return object
 }
