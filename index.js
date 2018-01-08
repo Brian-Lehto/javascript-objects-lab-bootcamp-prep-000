b7423927cf20175ad6d44fa7140073073048dc31
Brian  Lehto <bdlden1978@outlook.com>
2018-01-08 03:16:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 function deleteFromObjectByKey(object, key) {
-  return delete object[key]
+  return Object.assign({}, object);
 }
